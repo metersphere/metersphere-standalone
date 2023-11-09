59d185717ec1a6250868e750ac40eda16a3853d9
CaptainB <bin@fit2cloud.com>
2023-11-09 10:13:35 +0800

    build: task-runner shells

--- shells/task-runner.sh
+++ shells/task-runner.sh
@@ -4,5 +4,8 @@ export JAVA_CLASSPATH=/task-runner:/opt/jmeter/lib/ext/*:/task-runner/lib/*:/sta
 export JAVA_MAIN_CLASS=io.metersphere.Application
 export JAVA_OPTIONS="-Dfile.encoding=utf-8 -Djava.awt.headless=true --add-opens java.base/jdk.internal.loader=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED"
 
+node_exporter --path.procfs=/host/proc --path.sysfs=/host/sys &
+sed -i "s/:101:/:${MS_DOCKER_GID:-101}:/g" /etc/group
+
 sh /deployments/run-java.sh
 
