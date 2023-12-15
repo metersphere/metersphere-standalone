6419c7da56f8e72025bb863407e7d31b168b2734
CaptainB <bin@fit2cloud.com>
2023-12-15 13:40:18 +0800

    ci: package name changed

--- shells/task-runner.sh
+++ shells/task-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export JAVA_CLASSPATH=/task-runner:/opt/jmeter/lib/ext/*:/task-runner/lib/*:/standalone/lib/*
-export JAVA_MAIN_CLASS=io.metersphere.Application
+export JAVA_MAIN_CLASS=io.metersphere.runner.Application
 export JAVA_OPTIONS="-Dfile.encoding=utf-8 -Djava.awt.headless=true --add-opens java.base/jdk.internal.loader=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED"
 export MS_VERSION=`cat /tmp/MS_VERSION`
 
