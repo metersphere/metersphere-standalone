94dff60d8013da63ce66c963effec749fe9e36e6
CaptainB <bin@fit2cloud.com>
2024-01-17 20:58:31 +0800

    ci: 监控三个进程是否存在，不存在重启

---
+++ shells/daemon.sh
@@ -0,0 +1,36 @@
+while true; do
+    ps | grep metersphere.sh | grep -v grep | awk '{print $1}' >/tmp/metersphere.pid
+
+    if [ -z $(cat /tmp/metersphere.pid) ]; then
+        echo 'metersphere exited.'
+        echo ''>/tmp/metersphere.pid
+        break
+    fi
+
+    ps | grep task-runner.sh | grep -v grep | awk '{print $1}' >/tmp/task-runner.pid
+
+    if [ -z $(cat /tmp/task-runner.pid) ]; then
+        echo 'task-runner exited.'
+        echo ''>/tmp/task-runner.pid
+        break
+    fi
+
+    ps | grep result-hub.sh | grep -v grep | awk '{print $1}' >/tmp/result-hub.pid
+    if [ -z $(cat /tmp/result-hub.pid) ]; then
+        echo 'result-hub exited.'
+        echo ''>/tmp/result-hub.pid
+        break
+    fi
+
+    sleep 5
+done
+
+if [ -n $(cat /tmp/result-hub.pid) ]; then
+    kill -9 $(cat /tmp/result-hub.pid)
+fi
+if [ -n $(cat /tmp/metersphere.pid) ]; then
+    kill -9 $(cat /tmp/metersphere.pid)
+fi
+if [ -n $(cat /tmp/task-runner.pid) ]; then
+    kill -9 $(cat /tmp/task-runner.pid)
+fi
--- shells/start.sh
+++ shells/start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-sh /shells/metersphere.sh & sh /shells/task-runner.sh & sh /shells/result-hub.sh
+sh /shells/metersphere.sh & 
+sh /shells/task-runner.sh & 
+sh /shells/result-hub.sh  &
+sh /shells/daemon.sh      &
+
+
+wait
 
 
 
