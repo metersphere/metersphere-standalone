c1be046723926332bf49279f112d9489b5bd0f86
CaptainB <bin@fit2cloud.com>
2024-02-04 10:58:56 +0800

    ci: 去掉重复的配置，外部的可以覆盖这里

--- shells/result-hub.sh
+++ shells/result-hub.sh
@@ -2,7 +2,6 @@
 
 export JAVA_CLASSPATH=/result-hub:/opt/jmeter/lib/ext/*:/result-hub/lib/*:/standalone/lib/*:/server/lib/*
 export JAVA_MAIN_CLASS=io.metersphere.result.Application
-export JAVA_OPTIONS="-Dfile.encoding=utf-8 -Djava.awt.headless=true --add-opens java.base/jdk.internal.loader=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED"
 export MS_VERSION=`cat /tmp/MS_VERSION`
 
 sh /deployments/run-java.sh
